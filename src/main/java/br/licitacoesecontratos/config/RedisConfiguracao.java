508849e0f22e1e06caacf714faa5d27780871be5
Matias <mathias.ribeiro@gmail.com>
2021-05-11 14:01:02 -0300

    Configuração do redis para performance de consultas

---
+++ src/main/java/br/licitacoesecontratos/config/RedisConfiguracao.java
@@ -0,0 +1,119 @@
+package br.licitacoesecontratos.config;
+
+
+import java.net.URI;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.X509Certificate;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.data.redis.LettuceClientConfigurationBuilderCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.lettuce.core.RedisClient;
+import io.lettuce.core.RedisURI;
+import io.lettuce.core.api.StatefulRedisConnection;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
+
+@Configuration
+public class RedisConfiguracao {
+
+	@Value("${spring.redis.url}")
+    private static String REDIS_URL;
+	
+    @Bean
+    public LettuceClientConfigurationBuilderCustomizer lettuceClientConfigurationBuilderCustomizer() {
+        return clientConfigurationBuilder -> {
+            if (clientConfigurationBuilder.build().isUseSsl()) {
+                clientConfigurationBuilder.useSsl().disablePeerVerification();
+            }
+        };
+    }
+    
+    public static StatefulRedisConnection<String, String> connect() {
+        RedisURI redisURI = RedisURI.create(System.getenv(REDIS_URL));
+        redisURI.setVerifyPeer(false);
+
+        RedisClient redisClient = RedisClient.create(redisURI);
+        return redisClient.connect();
+    }
+    
+	
+    private static Jedis getConnection() {
+        try {
+            TrustManager bogusTrustManager = new X509TrustManager() {
+                public X509Certificate[] getAcceptedIssuers() {
+                    return null;
+                }
+
+                public void checkClientTrusted(X509Certificate[] certs, String authType) {
+                }
+
+                public void checkServerTrusted(X509Certificate[] certs, String authType) {
+                }
+            };
+
+            SSLContext sslContext = SSLContext.getInstance("SSL");
+            sslContext.init(null, new TrustManager[]{bogusTrustManager}, new java.security.SecureRandom());
+
+            HostnameVerifier bogusHostnameVerifier = (hostname, session) -> true;
+
+            return new Jedis(URI.create(System.getenv(REDIS_URL)),
+                    sslContext.getSocketFactory(),
+                    sslContext.getDefaultSSLParameters(),
+                    bogusHostnameVerifier);
+
+        } catch (NoSuchAlgorithmException | KeyManagementException e) {
+            throw new RuntimeException("Cannot obtain Redis connection!", e);
+        }
+    }
+    
+    public static JedisPool getPool() {
+        try {
+            TrustManager bogusTrustManager = new X509TrustManager() {
+                public X509Certificate[] getAcceptedIssuers() {
+                    return null;
+                }
+
+                public void checkClientTrusted(X509Certificate[] certs, String authType) {
+                }
+
+                public void checkServerTrusted(X509Certificate[] certs, String authType) {
+                }
+            };
+
+            SSLContext sslContext = SSLContext.getInstance("SSL");
+            sslContext.init(null, new TrustManager[]{bogusTrustManager}, new java.security.SecureRandom());
+
+            HostnameVerifier bogusHostnameVerifier = (hostname, session) -> true;
+
+            JedisPoolConfig poolConfig = new JedisPoolConfig();
+            poolConfig.setMaxTotal(10);
+            poolConfig.setMaxIdle(5);
+            poolConfig.setMinIdle(1);
+            poolConfig.setTestOnBorrow(true);
+            poolConfig.setTestOnReturn(true);
+            poolConfig.setTestWhileIdle(true);
+
+            return new JedisPool(poolConfig,
+                    URI.create(System.getenv(REDIS_URL)),
+                    sslContext.getSocketFactory(),
+                    sslContext.getDefaultSSLParameters(),
+                    bogusHostnameVerifier);
+
+        } catch (NoSuchAlgorithmException | KeyManagementException e) {
+            throw new RuntimeException("Cannot obtain Redis connection!", e);
+        }
+    }
+    
+    
+    
+}
--- src/main/java/br/licitacoesecontratos/controller/LicitacoesGovController.java
+++ src/main/java/br/licitacoesecontratos/controller/LicitacoesGovController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.Cacheable;
+import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,15 +19,20 @@ import br.licitacoesecontratos.repository.ILicitacoesGovRepositorio;
 
 @RestController
 @RequestMapping(value = "/licitacoes")
-@CrossOrigin(origins = "*", maxAge = 3600)
+@CrossOrigin(origins = "*")
 public class LicitacoesGovController {
 
 	@Autowired
 	private ILicitacoesGovRepositorio licitacoesRepositorio;
 	
+	@Autowired
+	RedisCacheManager redisCacheManager;
+	
+	
 	@GetMapping
-	@Cacheable(value = "listarTodasLicitacoes")
+	@Cacheable(value = "listarTodasLicitacoes", key="#root.method.name")
 	public List<LicitacoesGovForm> listarTodas() throws JsonProcessingException {
+		
 		
 		List<Licitacoes> licitacoes = licitacoesRepositorio.findAll();
 		List<LicitacoesGovForm> licitacaoGovForm = new LicitacoesGovForm().converter(licitacoes);
